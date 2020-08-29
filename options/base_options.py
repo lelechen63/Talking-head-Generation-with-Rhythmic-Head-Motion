029e50d68753e389a8c5e699605183565576cd85
rainingDesert <gcui2@ur.rochester.edu>
2020-08-29 15:24:26 -0400

    option_update

--- options/base_options.py
+++ options/base_options.py
@@ -34,8 +34,8 @@ class BaseOptions():
         parser.add_argument('--output_nc', type=int, default=3, help='# of output image channels')
 
         # for setting inputs
-        parser.add_argument('--dataroot', type=str, default='datasets/pose/')
-        parser.add_argument('--dataset_mode', type=str, default='fewshot_pose')        
+        parser.add_argument('--dataroot', type=str, default='voxceleb2')
+        parser.add_argument('--dataset_mode', type=str, default='facefore')        
         parser.add_argument('--resize_or_crop', type=str, default='scale_width', help='scaling and cropping of images at load time [resize_and_crop|crop|scale_width|scale_width_and_crop]')
         parser.add_argument('--serial_batches', action='store_true', help='if true, takes images in order to make batches, otherwise takes them randomly')        
         parser.add_argument('--no_flip', action='store_true', help='if specified, do not flip the images for data argumentation') 
